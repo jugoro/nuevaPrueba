4b96dc831dd85e23d92c01f42258e441a7c270ea
Juan <jugoro.0705@gmail.com>
2023-03-24 10:37:52 +0100

    paso 1

--- src/app/nuevo-componente/nuevo-componente.component.ts
+++ src/app/nuevo-componente/nuevo-componente.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nuevo-componente',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./nuevo-componente.component.css']
 })
 export class NuevoComponenteComponent {
-
+  @Input() name: string = "";
 }
